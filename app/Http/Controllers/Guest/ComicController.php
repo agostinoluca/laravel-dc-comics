e3c91c7bfb8ac92786a7f2f8e2c4c99bd651cc40
agostinoluca <agostino.luca89@gmail.com>
2024-05-02 18:24:49 +0200

    move ComicController in folder Guest

--- app/Http/Controllers/Guest/ComicController.php
+++ app/Http/Controllers/Guest/ComicController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Guest;
 
 use App\Models\Comic;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ComicController extends Controller
 {
